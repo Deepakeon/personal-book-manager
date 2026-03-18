44856282de0d630ffe3f8b75447144ada4f34f57
Deepakeon <eonzolt@gmail.com>
2026-03-18 16:38:47 +0530

    added back vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
       <body
         className={`${playfair.variable} ${dmSans.variable} antialiased`}
       >
-        {/* <Analytics/> */}
+        <Analytics/>
         <Toaster />
         {children}
       </body>
